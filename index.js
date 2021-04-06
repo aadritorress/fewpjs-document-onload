28cb8836c161de1f5fc3bbc981c1568322e58fb9
Adriana <adriana.amaral@msn.com>
2021-04-06 17:17:42 -0500

    commit tree 02e86fd434a891ca9233c1f53dbc08ca67b06b1a

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded");
+
+  let replace = document.querySelector("#text")
+    replace.innerHTML = "This is really cool!" 
+
+
+});
+
